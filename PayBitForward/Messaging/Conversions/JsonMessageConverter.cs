2e0fbfc69fc988112aaf76e24b8cb56ac4c5a79a
swede <swede.struve@aggiemail.usu.edu>
2017-11-28 20:34:41 -0700

    Added message ids to deserialization

--- PayBitForward/Messaging/Conversions/JsonMessageConverter.cs
+++ PayBitForward/Messaging/Conversions/JsonMessageConverter.cs
@@ -30,6 +30,10 @@ namespace PayBitForward.Messaging
                     return JsonConvert.DeserializeObject<ChunkReply>(str);
                 case MessageType.REGISTER_CONTENT_REQUEST:
                     return JsonConvert.DeserializeObject<RegisterContentRequest>(str);
+                case MessageType.CONTENT_LIST_REQUEST:
+                    return JsonConvert.DeserializeObject<ContentListRequest>(str);
+                case MessageType.CONTENT_LIST_REPLY:
+                    return JsonConvert.DeserializeObject<ContentListReply>(str);
                 default:
                     Log.ErrorFormat("Can not deserialize bytes with MessageId of {0}", mesg.MessageId);
                     throw new Exception("Can not deserialize bytes");
